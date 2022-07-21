5d9a8018e06f8aa04d26be44896a2f29a11e9f10
major-menphis <ssmenphis@hotmail.com>
2022-07-21 15:01:28 -0300

    add behavior

--- intro_Poo_python/rock-paper-scissor.py
+++ intro_Poo_python/rock-paper-scissor.py
@@ -3,16 +3,49 @@ class Participant:
         self.name = name
         self.points = 0
         self.choice = ""
+
     def choose(self):
         self.choice = input(f"{self.name}, select rock, paper or scissor: ")
-        print(f"{self.name} selects {self.choice}")
+        print(f"{self.name} selects {self.choice}.")
+
+    def toNumericalChoice(self):
+        switcher = {
+            "rock": 0,
+            "paper": 1,
+            "scissor": 2
+        }
+        return switcher[self.choice]
+
+    def incrementPoint(self):
+        self.points += 1
 
 class GameRound:
     def __init__(self, p1, p2):
+        self.rules = [
+            [0, -1, 1],
+            [1, 0, -1],
+            [-1, 1, 0]
+        ]
         p1.choose()
         p2.choose()
-    def compareChoices(self):
-        print("implement")
+        result = self.compareChoices(p1,p2)
+        print(f"Round resulted in a {self.getResultAsString(result)}.")
+        if result > 0:
+           p1.incrementPoint()
+        elif result < 0:
+           p2.incrementPoint()
+
+    def compareChoices(self, p1, p2):
+        return self.rules[p1.toNumericalChoice()][p2.toNumericalChoice()]
+
+    def getResultAsString(self, result):
+        res = {
+            0: "draw",
+            1: "win",
+            -1: "loss"
+        }       
+        return res[result]
+
     def awardPoints(self):
         print("implement")
 
@@ -21,13 +54,29 @@ class Game:
         self.endGame = False
         self.participant = Participant("Spock")
         self.secondParticipant = Participant("Kirk")
+
     def start(self):
-        game_round = GameRound(self.participant, self.secondParticipant)
+        while not self.endGame:
+            GameRound(self.participant, self.secondParticipant)
+            self.checkEndCondition()
 
     def checkEndCondition(self):
-        print("implement")
+        answer = input("Continue game y/n: ")
+        if answer == 'y':
+            GameRound(self.participant, self.secondParticipant)
+            self.checkEndCondition()
+        else:
+            print(f"Game ended, {self.participant.name} has {self.participant.points}, and {self.secondParticipant.name} has {self.secondParticipant.points}.")
+            self.determineWinner()
+            self.endGame = True
+
     def determineWinner(self):
-        print("implement")
+        resultString = "It's a Draw"
+        if self.participant.points > self.secondParticipant.points:
+            resultString = f"Winner is {self.participant.name}."
+        elif self.participant.points < self.secondParticipant.points:
+            resultString = f"Winner is {self.secondParticipant.name}."
+        print(resultString)
 
 game = Game()
 game.start()
